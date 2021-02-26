2cd569c7d9450f09da808c6397b9f55f1378d518
maegan <maegan@maeganwilson.com>
2021-02-26 07:55:35 -0600

    Top left is looking like the image

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Made by @maeganwilson_ on{" "}
+        <a href="https://twitch.tv/maeganwilson_">Twitch.tv/maeganwilson_</a>
+      </p>
+    </footer>
+  );
+};
+export default Footer;
