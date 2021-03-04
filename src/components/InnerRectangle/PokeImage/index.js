2756a2dd48d0dd04683ccd2efeb970a88d7e7ba1
maegan <maegan@maeganwilson.com>
2021-03-04 07:33:19 -0600

    Working on the image section of the pokedex

---
+++ src/components/InnerRectangle/PokeImage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./index.css";
+
+export default class PokeImage extends Component {
+  render() {
+    return (
+      <div className="PokeImageBox">
+        IMAGE WILL BE HERE
+      </div>
+    )
+  }
+}
--- src/components/InnerRectangle/index.js
+++ src/components/InnerRectangle/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./LeftSide.css";
+import PokeImage from "./PokeImage";
 
 export default function LeftSide() {
   return (
     <div className="background">
+      <PokeImage/>
     </div>
   )
 }
